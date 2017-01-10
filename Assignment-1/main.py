3b1222f478481f1466a6eaf8c1dfe6f33c561102
B M Abir <B M Abir>
2017-01-10 18:51:48 +0600

    10.1.17

--- Assignment-1/main.py
+++ Assignment-1/main.py
@@ -1,4 +1,4 @@
-print "Lexical Analyzer by B M Abir 12201022"
+print ("Lexical Analyzer by B M Abir 12201022")
 inFile= open('input.txt','r')
 word_list=[]
 
@@ -7,14 +7,26 @@ word_list=[]
 
 
 def removeSpace(inFile):
-    
+    tempWord=' '
+    i=0
     for line in inFile:
         
-        print 'line1'
+        ##print ('line1')
         for char in line:
-            print char
-            word_list=word_list+char
+            ##print (char)
+            
+            if(char==' ' or char=='\n' or char==',' or char==';'):
+                ##print(tempWord)
+                tempWord=tempWord.strip()
+                tempWord=tempWord.strip('\n')
+                word_list.insert(len(word_list),tempWord)
+                ##print(word_list)
+                tempWord=' '
+
+                
+            tempWord+=char
 
 
 
 removeSpace(inFile)
+print(word_list)
